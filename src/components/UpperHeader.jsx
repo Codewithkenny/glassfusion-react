ac2621f745de5a4eab30ae4db266ca2601a729b6
poju <pojuagbomeji@gmail.com>
2024-11-08 16:18:01 +0100

    added a button to upperheader

--- src/components/UpperHeader.jsx
+++ src/components/UpperHeader.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FaFacebookF, FaInstagram } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const UpperHeader = () => {
   return (
@@ -8,18 +9,24 @@ const UpperHeader = () => {
         {/* Left section: Phone, Contact Us */}
         <div className="flex space-x-4">
           <span>📞 +234 815 757 4317</span>
-          <a href="mailto:nfo@glassfusion.ng " className="hover:underline">
+          <a href="mailto:info@glassfusion.ng" className="hover:underline">
             Contact Us
           </a>
         </div>
-        {/* Right section: Social Links */}
-        <div className="flex space-x-4">
-        <a href="https://www.facebook.com/glassfusionafrica" target="_blank" rel="noopener noreferrer" className="hover:text-blue-600">
-          <FaFacebookF />
-        </a>
-        <a href="https://www.instagram.com/glass.fusion" target="_blank" rel="noopener noreferrer" className="hover:text-pink-600">
-          <FaInstagram />
-        </a>
+        
+        {/* Right section: Social Links and Get Quote Button */}
+        <div className="flex items-center space-x-4">
+          <a href="https://www.facebook.com/glassfusionafrica" target="_blank" rel="noopener noreferrer" className="hover:text-blue-600">
+            <FaFacebookF />
+          </a>
+          <a href="https://www.instagram.com/glass.fusion" target="_blank" rel="noopener noreferrer" className="hover:text-pink-600">
+            <FaInstagram />
+          </a>
+          <Link to="/contact">
+          <button className="bg-[#13084B] text-white px-4 py-1 rounded-full hover:bg-opacity-90 transition">
+            Get Quote
+          </button>
+          </Link>
         </div>
       </div>
     </div>
